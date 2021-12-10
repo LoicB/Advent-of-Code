89238788d754aaed88f4491cf65e568a52efd00d
LoicB <loic.beylot@ricoh-europe.com>
2021-12-10 18:34:21 +0000

    Days 10 2021

--- readme.md
+++ readme.md
@@ -1,16 +1,17 @@
 # 2021
 
-| Day                                                | Part One | Part Two |
-|----------------------------------------------------|----------|----------|
-| [Day 1](./src/main/java/aoc/loicb/y2021/Day1.java) | 345.7 μs | 759.2 μs |
-| [Day 2](./src/main/java/aoc/loicb/y2021/Day2.java) | 2.586 ms | 1.137 ms |
-| [Day 3](./src/main/java/aoc/loicb/y2021/Day3.java) | 2.182 ms | 5.123 ms |
-| [Day 4](./src/main/java/aoc/loicb/y2021/Day4.java) | 31.04 ms | 151.5 ms |
-| [Day 5](./src/main/java/aoc/loicb/y2021/Day5.java) | 85.80 ms | 87.24 ms |
-| [Day 6](./src/main/java/aoc/loicb/y2021/Day6.java) | 1.294 ms | 91.10 μs |
-| [Day 7](./src/main/java/aoc/loicb/y2021/Day7.java) | 35.16 ms | 33.75 ms |
-| [Day 8](./src/main/java/aoc/loicb/y2021/Day8.java) | 3.971 ms | 32.01 ms |
-| [Day 9](./src/main/java/aoc/loicb/y2021/Day9.java) | 57.86 ms | 41.34 ms |
+| Day                                                  | Part One | Part Two |
+|------------------------------------------------------|----------|----------|
+| [Day 1](./src/main/java/aoc/loicb/y2021/Day1.java)   | 345.7 μs | 759.2 μs |
+| [Day 2](./src/main/java/aoc/loicb/y2021/Day2.java)   | 2.586 ms | 1.137 ms |
+| [Day 3](./src/main/java/aoc/loicb/y2021/Day3.java)   | 2.182 ms | 5.123 ms |
+| [Day 4](./src/main/java/aoc/loicb/y2021/Day4.java)   | 31.04 ms | 151.5 ms |
+| [Day 5](./src/main/java/aoc/loicb/y2021/Day5.java)   | 85.80 ms | 87.24 ms |
+| [Day 6](./src/main/java/aoc/loicb/y2021/Day6.java)   | 1.294 ms | 91.10 μs |
+| [Day 7](./src/main/java/aoc/loicb/y2021/Day7.java)   | 35.16 ms | 33.75 ms |
+| [Day 8](./src/main/java/aoc/loicb/y2021/Day8.java)   | 3.971 ms | 32.01 ms |
+| [Day 9](./src/main/java/aoc/loicb/y2021/Day9.java)   | 57.86 ms | 41.34 ms |
+| [Day 10](./src/main/java/aoc/loicb/y2021/Day10.java) | 6.023 ms | 8.076 ms |
 
 # 2015
 
---
+++ src/test/java/aoc/loicb/y2021/Day10Test.java
@@ -0,0 +1,120 @@
+package aoc.loicb.y2021;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day10Test {
+    private static Stream<Arguments> partOneCaseProvider() {
+        return Stream.of(
+                Arguments.of(new String[]{
+                        "[({(<(())[]>[[{[]{<()<>>",
+                        "[(()[<>])]({[<{<<[]>>(",
+                        "{([(<{}[<>[]}>{[]{[(<()>",
+                        "(((({<>}<{<{<>}{[]{[]{}",
+                        "[[<[([]))<([[{}[[()]]]",
+                        "[{[{({}]{}}([{[{{{}}([]",
+                        "{<[[]]>}<{[{[{[]{()[[[]",
+                        "[<(<(<(<{}))><([]([]()",
+                        "<{([([[(<>()){}]>(<<{{",
+                        "<{([{{}}[<[[[<>{}]]]>[]]"
+                }, 26397)
+        );
+    }
+
+    private static Stream<Arguments> foundInvalidCharacterCaseProvider() {
+        return Stream.of(
+                Arguments.of("{([(<{}[<>[]}>{[]{[(<()>", '}'),
+                Arguments.of("[[<[([]))<([[{}[[()]]]", ')'),
+                Arguments.of("[{[{({}]{}}([{[{{{}}([]", ']'),
+                Arguments.of("[<(<(<(<{}))><([]([]()", ')'),
+                Arguments.of("<{([([[(<>()){}]>(<<{{", '>'),
+                Arguments.of("[({(<(())[]>[[{[]{<()<>>", null)
+        );
+    }
+
+    private static Stream<Arguments> partTwoCaseProvider() {
+        return Stream.of(
+                Arguments.of(new String[]{
+                        "[({(<(())[]>[[{[]{<()<>>",
+                        "[(()[<>])]({[<{<<[]>>(",
+                        "{([(<{}[<>[]}>{[]{[(<()>",
+                        "(((({<>}<{<{<>}{[]{[]{}",
+                        "[[<[([]))<([[{}[[()]]]",
+                        "[{[{({}]{}}([{[{{{}}([]",
+                        "{<[[]]>}<{[{[{[]{()[[[]",
+                        "[<(<(<(<{}))><([]([]()",
+                        "<{([([[(<>()){}]>(<<{{",
+                        "<{([{{}}[<[[[<>{}]]]>[]]"
+                }, 288957)
+        );
+    }
+
+    private static Stream<Arguments> getRemainingCharCaseProvider() {
+        return Stream.of(
+                Arguments.of("[({(<(())[]>[[{[]{<()<>>", "}}]])})]"),
+                Arguments.of("[(()[<>])]({[<{<<[]>>(", ")}>]})"),
+                Arguments.of("(((({<>}<{<{<>}{[]{[]{}", "}}>}>))))"),
+                Arguments.of("{<[[]]>}<{[{[{[]{()[[[]", "]]}}]}]}>"),
+                Arguments.of("<{([{{}}[<[[[<>{}]]]>[]]", "])}>")
+        );
+    }
+
+    private static Stream<Arguments> calculateScoreRemainingCaseProvider() {
+        return Stream.of(
+                Arguments.of("}}]])})]", 288957),
+                Arguments.of(")}>]})", 5566),
+                Arguments.of("}}>}>))))", 1480781),
+                Arguments.of("]]}}]}]}>", 995444),
+                Arguments.of("])}>", 294)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("partOneCaseProvider")
+    void partOne(String[] navigationSubsystem, int expectedScore) {
+        Day10 day = new Day10();
+        int score = day.partOne(navigationSubsystem);
+        assertEquals(expectedScore, score);
+    }
+
+    @ParameterizedTest
+    @MethodSource("foundInvalidCharacterCaseProvider")
+    void foundInvalidCharacter(String line, Character expectedInvalid) {
+        Day10 day = new Day10();
+        Optional<Character> invalid = day.foundInvalidCharacter(line);
+        assertEquals(Optional.ofNullable(expectedInvalid), invalid);
+    }
+
+    @ParameterizedTest
+    @MethodSource("partTwoCaseProvider")
+    void partTwo(String[] navigationSubsystem, int expectedScore) {
+        Day10 day = new Day10();
+        int score = day.partTwo(navigationSubsystem);
+        assertEquals(expectedScore, score);
+    }
+
+    @ParameterizedTest
+    @MethodSource("getRemainingCharCaseProvider")
+    void getRemainingChar(String line, String expectedRemaining) {
+        Day10 day = new Day10();
+        List<Character> remaining = day.getRemainingChar(line);
+        assertEquals(expectedRemaining, remaining.stream().map(character -> Character.toString(character)).collect(Collectors.joining("")));
+    }
+
+    @ParameterizedTest
+    @MethodSource("calculateScoreRemainingCaseProvider")
+    void calculateScoreRemaining(String remaining, long expectedScore) {
+        Day10 day = new Day10();
+        long score = day.calculateScoreRemaining(remaining.chars()
+                .mapToObj(e -> (char) e).collect(Collectors.toList()));
+        assertEquals(expectedScore, score);
+    }
+}
