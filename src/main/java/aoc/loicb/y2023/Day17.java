0e7756785ee5524946f9c3530484fc116afa796c
LoicB <loic.beylot@ricoh-europe.com>
2024-03-26 15:13:14 +0000

    Day 17 solution

--- src/main/java/aoc/loicb/y2023/Day17.java
+++ src/main/java/aoc/loicb/y2023/Day17.java
@@ -4,14 +4,12 @@ import aoc.loicb.Day;
 import aoc.loicb.DayExecutor;
 import aoc.loicb.InputToObjectList;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class Day17 implements Day<List<List<Integer>>, Number> {
     public static void main(String[] args) {
-        DayExecutor<List<List<Integer>>> de = new DayExecutor<>(new InputToObjectList<List<Integer>>() {
+        DayExecutor<List<List<Integer>>> de = new DayExecutor<>(new InputToObjectList<>() {
             @Override
             public List<Integer> transformObject(String string) {
                 return IntStream
@@ -26,93 +24,127 @@ public class Day17 implements Day<List<List<Integer>>, Number> {
 
     @Override
     public Number partOne(List<List<Integer>> input) {
-        return findMinimumHeat(input, new CrucibleState(0, 0, Direction.Right, 0));
+        return findMinimumHeat(input, new StateMotionEngine(input, false), new Crucible(0, 0, Direction.Right, 0));
     }
 
-    private Number findMinimumHeat(List<List<Integer>> input, Crucible startingCrucible) {
-        long[][] heatmap = new long[input.size()][];
-        for (int i = 0; i < input.size(); i++) {
-            heatmap[i] = new long[input.get(i).size()];
-            Arrays.fill(heatmap[i], Long.MAX_VALUE);
-        }
-        heatmap[0][0] = 0;
-
+    private Number findMinimumHeat(List<List<Integer>> input, StateMotionEngine stateMotionEngine, Crucible... startingCrucibles) {
         PriorityQueue<State> pqueue = new PriorityQueue<>();
-        pqueue.add(new State(startingCrucible, 0));
+        Set<Crucible> visited = new HashSet<>();
+        Arrays.stream(startingCrucibles).map(s -> new State(s, 0, null)).forEach(pqueue::add);
         while (!pqueue.isEmpty()) {
             State currentState = pqueue.poll();
-            Crucible cs = currentState.crucible();
-            long currentHeat = heatmap[cs.position().x()][cs.position().y()];
-            List<Crucible> nextSteps = cs.getNextStep();
-            for (Crucible nextStep : nextSteps) {
-                if (isInMap(nextStep, input)) {
-                    long newHeat = currentHeat + input.get(nextStep.position().x()).get(nextStep.position().y());
-                    if (newHeat < heatmap[nextStep.position().x()][nextStep.position().y()]) {
-                        heatmap[nextStep.position().x()][nextStep.position().y()] = newHeat;
-                        pqueue.add(new State(nextStep, newHeat));
+            if (!visited.contains(currentState.crucible())) {
+
+                visited.add(currentState.crucible());
+                List<State> nextSteps = stateMotionEngine.getNextStep(currentState);
+                for (State nextStep : nextSteps) {
+                    Crucible crucible = nextStep.crucible();
+                    pqueue.add(nextStep);
+                    if (crucible.position().x() == input.size() - 1
+                            && crucible.position().y() == input.get(crucible.position().x()).size() - 1
+                            && (!stateMotionEngine.ultraCrucible() || nextStep.crucible().cpt() >= 4)) {
+                        return nextStep.heat();
                     }
                 }
             }
         }
-        System.out.println(Arrays.deepToString(heatmap));
-        printheamap(heatmap);
-        return heatmap[input.size() - 1][input.getFirst().size() - 1];
-    }
-
-    private void printheamap(long[][] heatmap) {
-        for (int i = 0; i < heatmap.length; i++) {
-            for (int j = 0; j < heatmap[i].length; j++) {
-                if (heatmap[i][j] == Long.MAX_VALUE) {
-                    System.out.print("|  -");
-                } else if (heatmap[i][j] < 10) {
-                    System.out.print("|  " + heatmap[i][j]);
-                } else if (heatmap[i][j] < 100) {
-                    System.out.print("| " + heatmap[i][j]);
-                } else {
-                    System.out.print("|" + heatmap[i][j]);
-                }
-            }
-            System.out.println();
-        }
-
-    }
-
-    private boolean isInMap(Crucible crucibleState, List<List<Integer>> map) {
-        return crucibleState.position().x() >= 0
-                && crucibleState.position().x() < map.size()
-                && crucibleState.position().y() >= 0
-                && crucibleState.position().y() < map.get(crucibleState.position().x()).size();
+        return 0;
     }
 
     @Override
     public Number partTwo(List<List<Integer>> input) {
-        return findMinimumHeat(input, new UltraCrucibleState(0, 0, Direction.Right, 0));
+        return findMinimumHeat(input, new StateMotionEngine(input, true), new Crucible(0, 0, Direction.Right, 0), new Crucible(0, 0, Direction.Down, 0));
     }
 
-    interface Crucible {
-        Position position();
-
-        Direction direction();
-
-        int cpt();
-
-        List<Crucible> getNextStep();
-
-        boolean isEnoughCpt();
+    record Crucible(Position position, Direction direction, int cpt) {
+        Crucible(int x, int y, Direction direction, int cpt) {
+            this(new Position(x, y), direction, cpt);
+        }
     }
 
-    record State(Crucible crucible, long heat) implements Comparable<State> {
+    record StateMotionEngine(List<List<Integer>> heatmap, boolean ultraCrucible) {
+
+        public List<State> getNextStep(State state) {
+            return getNextStep(state.crucible(), this.ultraCrucible())
+                    .stream()
+                    .filter(crucible -> isInMap(crucible, heatmap))
+                    .map(crucible -> getState(state, crucible)).toList();
+        }
+
+        private boolean isInMap(Crucible crucibleState, List<List<Integer>> map) {
+            return crucibleState.position().x() >= 0
+                    && crucibleState.position().x() < map.size()
+                    && crucibleState.position().y() >= 0
+                    && crucibleState.position().y() < map.get(crucibleState.position().x()).size();
+        }
+
+
+        private State getState(State state, Crucible crucible) {
+            return new State(crucible, state.heat() + heatmap.get(crucible.position().x()).get(crucible.position().y()), state);
+        }
+
+        private List<Crucible> getNextStep(Crucible crucible, boolean ultraCrucible) {
+            if (ultraCrucible) return getUltraCrucibleNextStep(crucible);
+            return getSimpleCrucibleNextStep(crucible);
+        }
+
+        private List<Crucible> getSimpleCrucibleNextStep(Crucible crucible) {
+            if (crucible.position().x() == 0 && crucible.position().y() == 0) return List.of(goStraight(crucible));
+            if (crucible.cpt() == 3) return List.of(turnLeft(crucible), turnRight(crucible));
+            return List.of(goStraight(crucible), turnLeft(crucible), turnRight(crucible));
+        }
+
+        private List<Crucible> getUltraCrucibleNextStep(Crucible crucible) {
+            if (crucible.cpt() < 4) return List.of(goStraight(crucible));
+            if (crucible.cpt() < 10) return List.of(goStraight(crucible), turnLeft(crucible), turnRight(crucible));
+            return List.of(turnLeft(crucible), turnRight(crucible));
+        }
+
+        private Crucible goStraight(Crucible currentStep) {
+            return move(currentStep.position(), currentStep.direction(), currentStep.cpt() + 1);
+        }
+
+        private Crucible turnLeft(Crucible currentStep) {
+            return move(currentStep.position(), turnLeft(currentStep.direction()), 1);
+        }
+
+        private Crucible turnRight(Crucible currentStep) {
+            return move(currentStep.position(), turnRight(currentStep.direction()), 1);
+        }
+
+
+        private Crucible move(Position position, Direction direction, int cpt) {
+            return new Crucible(
+                    position.x() + direction.x(),
+                    position.y() + direction.y(),
+                    direction,
+                    cpt);
+        }
+
+
+        private Direction turnLeft(Direction currentDirection) {
+            return switch (currentDirection) {
+                case Left -> Direction.Down;
+                case Down -> Direction.Right;
+                case Right -> Direction.Up;
+                case Up -> Direction.Left;
+            };
+        }
+
+        private Direction turnRight(Direction currentDirection) {
+            return switch (currentDirection) {
+                case Left -> Direction.Up;
+                case Up -> Direction.Right;
+                case Right -> Direction.Down;
+                case Down -> Direction.Left;
+            };
+        }
+
+    }
+
+    record State(Crucible crucible, long heat, State previous) implements Comparable<State> {
         @Override
         public int compareTo(State other) {
-//            long ret = heat - other.heat();
-//            if (ret == 0 && crucible.direction() == other.crucible.direction()) {
-//                ret = crucible.cpt() - other.crucible.cpt();
-//            }
-//            if (ret == 0) {
-//                ret = other.crucible.position().x() + other.crucible.position().y() - crucible.position().x()  - crucible.position().y();
-//            }
-//            return (int) ret;
-//            return -1 * Long.compare(this.heat(), other.heat());
             if (this.heat() != other.heat()) {
                 return Long.compare(this.heat(), other.heat());
             } else if (this.crucible().direction() == other.crucible().direction() && this.crucible().cpt() != other.crucible().cpt()) {
@@ -123,125 +155,9 @@ public class Day17 implements Day<List<List<Integer>>, Number> {
                 return Integer.compare(this.crucible().position().x(), other.crucible().position().x());
             }
         }
+
     }
 
     record Position(int x, int y) {
     }
-
-    record CrucibleState(Position position, Direction direction, int cpt) implements Crucible {
-        CrucibleState(int x, int y, Direction direction, int cpt) {
-            this(new Position(x, y), direction, cpt);
-        }
-
-        @Override
-        public List<Crucible> getNextStep() {
-            if (this.position().x() == 0 && this.position().y() == 0) return List.of(goStraight(this));
-            if (this.cpt() == 3) return List.of(turnLeft(this), turnRight(this));
-            return List.of(goStraight(this), turnLeft(this), turnRight(this));
-        }
-
-        @Override
-        public boolean isEnoughCpt() {
-            return true;
-        }
-
-        private Crucible goStraight(Crucible currentStep) {
-            return move(currentStep.position(), currentStep.direction(), currentStep.cpt() + 1);
-        }
-
-        private Crucible turnLeft(Crucible currentStep) {
-            return move(currentStep.position(), turnLeft(currentStep.direction()), 0);
-        }
-
-        private Crucible turnRight(Crucible currentStep) {
-            return move(currentStep.position(), turnRight(currentStep.direction()), 0);
-        }
-
-
-        private CrucibleState move(Position position, Direction direction, int cpt) {
-            return new CrucibleState(
-                    position.x() + direction.x(),
-                    position.y() + direction.y(),
-                    direction,
-                    cpt);
-        }
-
-
-        private Direction turnLeft(Direction currentDirection) {
-            return switch (currentDirection) {
-                case Left -> Direction.Down;
-                case Down -> Direction.Right;
-                case Right -> Direction.Up;
-                case Up -> Direction.Left;
-            };
-        }
-
-        private Direction turnRight(Direction currentDirection) {
-            return switch (currentDirection) {
-                case Left -> Direction.Up;
-                case Up -> Direction.Right;
-                case Right -> Direction.Down;
-                case Down -> Direction.Left;
-            };
-        }
-    }
-
-    record UltraCrucibleState(Position position, Direction direction, int cpt) implements Crucible {
-        UltraCrucibleState(int x, int y, Direction direction, int cpt) {
-            this(new Position(x, y), direction, cpt);
-        }
-
-        @Override
-        public List<Crucible> getNextStep() {
-            if (this.cpt() < 4) return List.of(goStraight(this));
-            if (this.cpt() < 10) return List.of(goStraight(this), turnLeft(this), turnRight(this));
-            return List.of(turnLeft(this), turnRight(this));
-        }
-
-        @Override
-        public boolean isEnoughCpt() {
-            return this.cpt() >= 4;
-        }
-
-        private Crucible goStraight(Crucible currentStep) {
-            return move(currentStep.position(), currentStep.direction(), currentStep.cpt() + 1);
-        }
-
-        private Crucible turnLeft(Crucible currentStep) {
-            return move(currentStep.position(), turnLeft(currentStep.direction()), 0);
-        }
-
-        private Crucible turnRight(Crucible currentStep) {
-            return move(currentStep.position(), turnRight(currentStep.direction()), 0);
-        }
-
-
-        private UltraCrucibleState move(Position position, Direction direction, int cpt) {
-            return new UltraCrucibleState(
-                    position.x() + direction.x(),
-                    position.y() + direction.y(),
-                    direction,
-                    cpt);
-        }
-
-
-        private Direction turnLeft(Direction currentDirection) {
-            return switch (currentDirection) {
-                case Left -> Direction.Down;
-                case Down -> Direction.Right;
-                case Right -> Direction.Up;
-                case Up -> Direction.Left;
-            };
-        }
-
-        private Direction turnRight(Direction currentDirection) {
-            return switch (currentDirection) {
-                case Left -> Direction.Up;
-                case Up -> Direction.Right;
-                case Right -> Direction.Down;
-                case Down -> Direction.Left;
-            };
-        }
-    }
-
 }
